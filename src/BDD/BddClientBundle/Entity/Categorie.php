648e00c5fcd329e3fa65c548421043f8669c04d6
juliette.parillot <jujulaurette@gmail.com>
2015-06-16 09:31:42 +0100

    sxsqd

---
+++ src/BDD/BddClientBundle/Entity/Categorie.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace BDD\BddClientBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Categorie
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="BDD\BddClientBundle\Entity\CategorieRepository")
+ */
+class Categorie
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="saison", type="string", length=255)
+     */
+    private $saison;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return Categorie
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set saison
+     *
+     * @param string $saison
+     * @return Categorie
+     */
+    public function setSaison($saison)
+    {
+        $this->saison = $saison;
+
+        return $this;
+    }
+
+    /**
+     * Get saison
+     *
+     * @return string 
+     */
+    public function getSaison()
+    {
+        return $this->saison;
+    }
+}
