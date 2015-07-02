509d016329a43b56c4f38facc2b799584ca9fd2f
juliette.parillot <jujulaurette@gmail.com>
2015-07-03 01:32:11 +0200

    zdz

---
+++ src/Administrator/Administration/AdminBundle/Controller/GererController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Administrator\Administration\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GererController extends Controller
+{
+    
+    public function suivitDesCommandesAction(){
+      
+    }
+
+	public function lesProduitsAction(){
+      
+    }
+
+    public function lesCategoriesAction(){
+      
+    }
+
+	public function lesNewsAction(){
+      
+    }
+	
+	public function lesRecettesAction(){
+      
+    }
+
+	public function lesCoordsClientsAction(){
+      
+    }
+
+
+}
