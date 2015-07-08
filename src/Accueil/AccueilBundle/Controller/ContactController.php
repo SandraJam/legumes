8a4d249a4c329c9bc8628659b651cc19200ac59d
SandraJam <sandra.duprep@gmail.com>
2015-07-08 11:35:49 +0200

    Mon compte

--- src/Accueil/AccueilBundle/Controller/ContactController.php
+++ src/Accueil/AccueilBundle/Controller/ContactController.php
@@ -35,6 +35,63 @@ class ContactController extends Controller
     return $this->render('AccueilAccueilBundle::contact.html.twig', array(
         'form' => $form->createView()
     ));
+  }
+
+  public function compteAction(){
+    $session = $this->getRequest()->getSession();
+    if ($session->get('users') != NULL){
+      $user = $this->getDoctrine()
+                    ->getRepository('BDDBddClientBundle:Utilisateurs')
+                    ->find($session->get('id'));
+      if ($user != NULL) {
+        return $this->render('AccueilAccueilBundle::compte.html.twig', array(
+            'user' => $user
+        ));
+      }else{
+        return $this->redirect($this->generateUrl('accueil_accueil_homepage'));
+      }
+    } else {
+      return $this->redirect($this->generateUrl('accueil_accueil_homepage'));
     }
+  }
+
+  public function modifCompteAction() {
+    // Revoir sécurité
+    $session = $this->getRequest()->getSession();
+    if ($session->get('users') != NULL){
+      $user = $this->getDoctrine()
+                    ->getRepository('BDDBddClientBundle:Utilisateurs')
+                    ->find($session->get('id'));
+      if ($user != NULL) {
+        $tryAgain=false;
+        if(count($_POST['telephone']) != 10 && preg_match("/^(\d\d\s){4}(\d\d)+\s$/",$_POST['telephone']) ){
+            $tryAgain=true;
+        }
+        if(count($_POST['codepostal']) != 5 && !preg_match("/^[0-9]{5,5}$/",$_POST['codepostal'])){
+            $tryAgain=true;
+        }
+        if ( !preg_match ( "/[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/", $_POST['mail'] ) ){
+            $tryAgain=true;
+        }
+        if (!$tryAgain){
+          if ($_POST['password'] != ""){
+            $user->setPwd($_POST['password']);
+          }
+          $user->setAdresse($_POST['adresse']);
+          $user->setVille($_POST['ville']);
+          $user->setCodePostal($_POST['codepostal']);
+          $user->setTel($_POST['telephone']);
+          $user->setEmail($_POST['mail']);
+          $em = $this->getDoctrine()->getManager();
+          $em->flush();
+        }
+        return $this->redirect($this->generateUrl('accueil_accueil_compte'));
+      }else{
+        return $this->redirect($this->generateUrl('accueil_accueil_homepage'));
+      }
+    } else {
+      return $this->redirect($this->generateUrl('accueil_accueil_homepage'));
+    }
+  }
 
 }
