8de2a5e76978ef7c157f364d2f6281605d1ac2be
julietteparillot <julietteparillot@yahoo.fr>
2015-07-22 20:05:46 +0200

    ajout de la page de gestion des histoires + routing + controlleur en cours

--- src/Administrator/Administration/AdminBundle/Controller/HistoireController.php
+++ src/Administrator/Administration/AdminBundle/Controller/HistoireController.php
@@ -9,7 +9,16 @@ use BDD\BddClientBundle\Entity\Utilisateurs;
 class HistoireController extends Controller
 {
 	public function pageAccueilGestionHistoireAction(){
-	
+		$session = $this->getRequest()->getSession();
+		if ( ($session->get('pren') != NULL) && (strtolower($session->get('typ')) == 'administrateur') ) {
+			$em = $this->getDoctrine()->getManager();
+			$repository = $em
+                        ->getRepository('BDDBddClientBundle:Histoire');
+            $listeHistoire=$repository->findAll();
+			return $this->render('AdministratorAdministrationAdminBundle:Gerer:gererHistoireAccueil.html.twig',array('listHistoire' => $listeHistoire));
+		}else{
+			return $this->redirect($this->generateUrl('accueil_accueil_homepage'));
+		}		
 	}
 
 }
